ca4dc9b4dd61bf6650620529561fd5e0177bfe74
Developer <dev@Pair2.local>
2015-07-16 09:24:38 +0800

    form workflow complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,5 @@
+include ArticlesHelper
+
 class ArticlesController < ApplicationController
 	
 	def index
@@ -7,5 +9,33 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
-	
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		flash.notice = "Article #{@article.title} created!"
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash.notice = "Article #{@article.title} deleted!"
+		redirect_to articles_path
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		flash.notice = "Article #{@article.title} updated!"
+		redirect_to article_path(@article)
+	end
 end
